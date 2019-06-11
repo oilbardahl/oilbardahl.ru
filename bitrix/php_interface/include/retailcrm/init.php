7ab863bd372853c768ac34d056c9ffab9c96bfa7
root <root@localhost.localdomain>
2019-06-11 18:20:30 +0300

    transfer full address from retailcrm to bitrix

--- bitrix/php_interface/include/retailcrm/init.php
+++ bitrix/php_interface/include/retailcrm/init.php
@@ -16,3 +16,34 @@ function retailCrmBeforeOrderSend($order, $arFields)
     return $order;
     //либо return false; и тогда данные отправлены в систему не будут
 }
+
+if(!function_exists('getPropertyByCode')) {
+    function getPropertyByCode($propertyCollection, $code)
+    {
+        foreach ($propertyCollection as $property)
+            {
+                if ($property->getField('CODE') == $code)
+                    {
+                        return $property;
+                    }
+            }
+    }
+}
+
+function retailCrmAfterOrderSave($order)
+{
+    $orderId = $order['externalId'];
+    
+    if ($orderId) {
+        $fullAddress = $order['customFields']['delivery_address'];
+        $orderB = \Bitrix\Sale\Order::load($orderId);
+        $propertyCollection = $orderB->getPropertyCollection();
+        $prop = getPropertyByCode($propertyCollection, 'SINGLE_LINE_ADDRESS');
+        if ($fullAddress != $prop->getValue()) {
+            $prop->setValue($fullAddress);
+            $orderB->save();
+        }
+    }
+    
+    return $order;
+}

37b6a65ec4e7b230c4028b8a5fd4c28b6d398200
Maxim <makcrx@analitika.online>
2019-07-31 22:29:47 +0300

    makcrx: Google Analytics integration for 1click form

--- retailcrm/oneclick.php
+++ retailcrm/oneclick.php
@@ -41,9 +41,18 @@ if (!is_phone($phone)) {
 }
 
 
+
 $response = $client->ordersCreate($order);
-var_dump($order);
-var_dump($response);
+
+if ($response->isSuccessful()) {
+    $orderId = $response['id'];
+    // var_dump($response);
+    
+    $client->ordersFixExternalIds([['id' => $orderId, 'externalId' => $orderId . 'A']]);
+    // var_dump($response);
+    
+    CustomGA::sendOrderSuccessEvent2ua($orderId . 'A');
+}
 
 function is_phone($phone) {
     if (strpos($phone, '@') !== false)
@@ -54,4 +63,66 @@ function is_phone($phone) {
         return false;
     
     return true;
+}
+
+
+function gaParseCookie() { 
+    if (isset($_COOKIE['_ga'])) { 
+        list($version,$domainDepth, $cid1, $cid2) = preg_split('[\.]', $_COOKIE["_ga"],4); 
+
+        $contents = array(
+                            'version' => $version,
+                            'domainDepth' => $domainDepth, 
+                            'cid' => $cid1.'.'.$cid2
+                         ); 
+        $cid = $contents['cid'];
+    }
+  
+    
+    return $cid;
+}
+
+class CustomGA
+{
+    private static function sendEvent2ua($ec,$ea,$el,$UA,$cid)
+    { 
+        $url="http://www.google-analytics.com/collect";
+        $data = array( 
+          'v' => 1, 
+          'tid' => $UA, 
+          'cid' => $cid, 
+          't' => 'event',
+          'ec' => $ec,
+          'ea' => $ea,
+          'el' => $el
+        );
+        
+        $content = http_build_query($data); // The body of the post must include exactly 1 URI encoded payload and must be no longer than 8192 bytes. See http_build_query.
+        $content = utf8_encode($content); // The payload must be UTF-8 encoded.
+        $user_agent = 'RoboDigital/1.0'; // Throwing in a user agent just for good measure.
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_USERAGENT, $user_agent);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-type: application/x-www-form-urlencoded'));
+        curl_setopt($ch,CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch,CURLOPT_POST, TRUE);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $content);
+        curl_exec($ch);
+        curl_close($ch);
+    }
+    
+    // отправка в Google Analytics события order:success
+    public static function sendOrderSuccessEvent2ua($orderId) {
+        $ua_client_id = gaParseCookie();
+        
+        $ec = 'Sent_form';
+        $ea = 'Success';
+        $el = $orderId;
+        $UA='UA-66142478-1';
+        $cid=$ua_client_id;
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/retailcrm/ga-log.txt', $orderId . ' : ' . $cid . PHP_EOL, FILE_APPEND);
+        self::sendEvent2ua($ec,$ea,$el,$UA,$cid);
+    }
+    
+    // если нужно добавить другие события, создаём функции для отправки, аналогичные sendOrderSuccessEvent2ua
 }
